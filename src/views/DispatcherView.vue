c4a84789a70c441ebecd94020bfab7966d4e3a79
julplo <julia.ploman@gmail.com>
2023-11-21 14:24:26 +0100

    klar

--- src/views/DispatcherView.vue
+++ src/views/DispatcherView.vue
@@ -1,18 +1,32 @@
 <template>
   <div id="orders">
     <div id="orderList">
-      <div v-for="(order, key) in orders" v-bind:key="'order'+key">
-        #{{ key }}: {{ order.orderItems.join(", ") }}
-      </div>
-      <button v-on:click="clearQueue">Clear Queue</button>
-    </div>
-    <div id="dots" v-bind:style="{ background: 'url(' + require('../../public/img/polacks.jpg')+ ')' }">
-        <div v-for="(order, key) in orders" v-bind:style="{ left: order.details.x + 'px', top: order.details.y + 'px'}" v-bind:key="'dots' + key">
-          {{ key }}
+      <div v-for="(order, key) in orders" :key="'order' + key"> 
+        <span class="tjock">#{{ key }}: </span>
+        <span v-if="orderItemsArray(order.orderItems).length > 0">
+          {{ orderItemsString(order.orderItems) }},
+        </span>
+        <div>
+          Skickas till: {{ order.orderNamn }} ({{ order.orderEmail }}, {{ order.orderKön }}) 
+          <br>
+          Betalar med: {{ order.orderPayment }}
+          <br>
+          <br>
         </div>
+      </div>
+      <button @click="clearQueue">Clear Queue</button>
+    </div>
+    <div id="dots" v-bind:style="{ background: 'url(' + require('../../public/img/polacks.jpg') + ')' }">
+      <div v-for="(order, key) in orders" v-bind:style="{
+        left: order.details.x + 'px',
+        top: order.details.y + 'px'
+      }" v-bind:key="'dots' + key">
+        {{ key }}
+      </div>
     </div>
   </div>
 </template>
+
 <script>
 import io from 'socket.io-client'
 const socket = io();
@@ -29,28 +43,43 @@ export default {
       this.orders = data.orders);
   },
   methods: {
+    orderItemsArray(orderItems) {
+      const itemsArray = [];
+      for (const itemName in orderItems) {
+        itemsArray.push({ name: itemName, quantity: orderItems[itemName] });
+      }
+      return itemsArray;
+    },
+
+    orderItemsString(orderItems) {
+      const itemsArray = this.orderItemsArray(orderItems);
+      return itemsArray.map(item => `${item.name} x ${item.quantity}`).join(', ');
+    },
+    
     clearQueue: function () {
       socket.emit('clearQueue');
-    }
+    },
   }
 }
 </script>
+
 <style>
 #orderList {
-  top:1em;
-  left:1em;
+  top: 1em;
+  left: 1em;
   position: absolute;
   z-index: 2;
-  color:black;
-  background: rgba(255,255,255, 0.5);
+  color: black;
+  background: rgba(255, 255, 255, 0.5);
   padding: 1em;
 }
+
 #dots {
   position: relative;
   margin: 0;
   padding: 0;
   background-repeat: no-repeat;
-  width:1920px;
+  width: 1920px;
   height: 1078px;
   cursor: crosshair;
 }
@@ -60,8 +89,11 @@ export default {
   background: black;
   color: white;
   border-radius: 10px;
-  width:20px;
-  height:20px;
+  width: 50px;
+  height: 20px;
   text-align: center;
+}
+  .tjock {
+   font-weight: bold;
 }
 </style>
